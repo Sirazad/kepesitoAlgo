17eaec5185d5a0f27b967996c90a6fd85618ed20
Sirazad <lajosimola2@gmail.com>
2021-04-29 10:21:16 +0200

    run all tests green

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,14 @@
 package hu.nive.ujratervezes.adddigits;
 
+import java.util.Arrays;
+
 public class AddDigits {
     //todo: implement your solution here
+
+    public int addDigits(String input) {
+        if (input == null || input.length() == 0) return -1;
+        input = input.replaceAll("\\D", "");
+        if (input.equals("")) return 0;
+        return Arrays.stream(input.split("")).mapToInt(digit -> Integer.parseInt(digit)).reduce(0, Integer::sum);
+    }
 }
